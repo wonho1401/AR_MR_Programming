5e3368d2e778fe8cc1f77d112cf32e7caa2f293b
wonho <wonho1401@naver.com>
2022-12-11 22:10:43 +0900

    pipe valves code
    pipe valves code

---
+++ unityProg/Assets/Scenes/Scripts/FlowManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowManager : MonoBehaviour
+{
+    public GameObject[] GuideSteps;
+    public Transform StepHolder;
+
+    private int currentStep = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetFlow();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeStep(bool isNext)
+    {
+        if (isNext)
+        {
+            if(currentStep >= GuideSteps.Length - 1)
+            {
+                return;
+            }
+            currentStep++;
+        }
+        else
+        {
+            if(currentStep <= 0)
+            {
+                return;
+            }
+            currentStep--;
+        }
+        GuideSteps[currentStep].SetActive(true);
+    }
+
+    public void ResetFlow()
+    {
+        HideAllSteps();
+        currentStep = 0;
+        GuideSteps[0].SetActive(true);
+    }
+
+    void HideAllSteps()
+    {
+        foreach(Transform child in StepHolder)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+}
